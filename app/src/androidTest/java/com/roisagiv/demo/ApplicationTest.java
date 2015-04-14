35e11339a86dcd1ec15e84e1cca2301a1a8bf4f4
Roi Sagiv <roisagiv@gmail.com>
2015-04-14 15:22:38 +0300

    rename package to com.roisagiv.demo

--- app/src/androidTest/java/com/roisagiv/demo/ApplicationTest.java
+++ app/src/androidTest/java/com/roisagiv/demo/ApplicationTest.java
@@ -1,4 +1,4 @@
-package demo.roisagiv.com;
+package com.roisagiv.demo;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/roisagiv/demo/MainActivity.java
+++ app/src/main/java/com/roisagiv/demo/MainActivity.java
@@ -1,4 +1,4 @@
-package demo.roisagiv.com;
+package com.roisagiv.demo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
