0bb769fe088b4fe62c78777d32bdca6c2f6ac9cb
Roi Sagiv <roisagiv@gmail.com>
2015-04-20 19:45:00 +0300

    GetUsersLoaderTest tests pass with mockito

--- app/src/androidTest/java/com/roisagiv/demo/users/GetUsersLoaderTest.java
+++ app/src/androidTest/java/com/roisagiv/demo/users/GetUsersLoaderTest.java
@@ -2,11 +2,19 @@ package com.roisagiv.demo.users;
 
 import android.test.AndroidTestCase;
 import java.util.List;
+import org.mockito.Mockito;
 
 /**
  *
  */
 public class GetUsersLoaderTest extends AndroidTestCase {
+
+  @Override protected void setUp() throws Exception {
+    super.setUp();
+
+    // required by mockito
+    System.setProperty("dexmaker.dexcache", mContext.getCacheDir().getPath());
+  }
 
   public void test_loadInBackground_should_call_UsersAPIClient() {
     // arrange
@@ -18,6 +26,18 @@ public class GetUsersLoaderTest extends AndroidTestCase {
 
     // assert
     assertTrue(usersAPIClient.getUsers_invoked);
+  }
+
+  public void test_loadInBackground_should_call_UsersAPIClient_with_mockito() {
+    // arrange
+    UsersAPIClient mockedUsersAPIClient = Mockito.mock(UsersAPIClient.class);
+    GetUsersLoader loader = new GetUsersLoader(getContext(), mockedUsersAPIClient);
+
+    // act
+    loader.loadInBackground();
+
+    // assert
+    Mockito.verify(mockedUsersAPIClient).getUsers();
   }
 
   private static class TestableUsersAPIClient implements UsersAPIClient {
