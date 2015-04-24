250a3eecbec4eb67babc01d3e23b22552b33cec4
Roi Sagiv <roisagiv@gmail.com>
2015-04-24 18:51:24 +0300

    UsersListActivityEspressoTest tests pass

--- app/src/main/java/com/roisagiv/demo/users/UsersListActivity.java
+++ app/src/main/java/com/roisagiv/demo/users/UsersListActivity.java
@@ -1,14 +1,19 @@
 package com.roisagiv.demo.users;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 import com.roisagiv.demo.R;
+import com.roisagiv.demo.utils.PicassoImageDownloader;
+import java.util.List;
 
-public class UsersListActivity extends ActionBarActivity {
+public class UsersListActivity extends ActionBarActivity
+    implements LoaderManager.LoaderCallbacks<List<User>> {
+
+  private ListUsersRecyclerViewAdapter listUsersRecyclerViewAdapter;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -16,5 +21,22 @@ public class UsersListActivity extends ActionBarActivity {
 
     RecyclerView usersRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_users);
     usersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    listUsersRecyclerViewAdapter = new ListUsersRecyclerViewAdapter(new PicassoImageDownloader());
+    usersRecyclerView.setAdapter(listUsersRecyclerViewAdapter);
+
+    getSupportLoaderManager().initLoader(0, null, this);
+  }
+
+  @Override public Loader<List<User>> onCreateLoader(int id, Bundle args) {
+    return new GetUsersLoader(this, new LocalFileUsersAPIClient(getAssets()));
+  }
+
+  @Override public void onLoadFinished(Loader<List<User>> loader, List<User> data) {
+    listUsersRecyclerViewAdapter.setUsers(data);
+  }
+
+  @Override public void onLoaderReset(Loader<List<User>> loader) {
+
   }
 }
